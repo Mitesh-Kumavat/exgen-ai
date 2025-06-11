db36f25206716a3b19786dc4077ef7c9620d3f8c
Mitesh-Kumavat <miteshkumavat777@gmail.com>
2025-06-11 22:06:15 +0530

    feat: add basic frontend setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import Login from './pages/Login.tsx'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path='/' element={<App />}>
+      </Route>
+      <Route path='/login' element={<Login />} />
+    </>
+  )
+)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
